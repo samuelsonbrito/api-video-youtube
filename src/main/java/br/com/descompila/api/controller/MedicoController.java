ae3c13d554d3607075e830e3abde6139d8d3c6e9
Samuelson <samuelsonma@gmail.com>
2024-04-13 09:13:18 -0400

    respository

--- src/main/java/br/com/descompila/api/controller/MedicoController.java
+++ src/main/java/br/com/descompila/api/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package br.com.descompila.api.controller;
 
 import br.com.descompila.api.medico.DadosCadastroMedico;
+import br.com.descompila.api.medico.Medico;
+import br.com.descompila.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+
+        repository.save(new Medico(dados));
     }
 }
--- src/main/java/br/com/descompila/api/medico/Medico.java
+++ src/main/java/br/com/descompila/api/medico/Medico.java
@@ -18,9 +18,17 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
-    private String crn;
-    @Enumerated
+    private String crm;
+    @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
