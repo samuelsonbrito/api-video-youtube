320a7be21a91eb497294d5f7d883e23128389365
Samuelson <samuelsonma@gmail.com>
2024-04-20 20:26:33 -0400

    criando listagem de medicos

--- src/main/java/br/com/descompila/api/controller/MedicoController.java
+++ src/main/java/br/com/descompila/api/controller/MedicoController.java
@@ -1,15 +1,15 @@
 package br.com.descompila.api.controller;
 
 import br.com.descompila.api.medico.DadosCadastroMedico;
+import br.com.descompila.api.medico.DadosListagemMedico;
 import br.com.descompila.api.medico.Medico;
 import br.com.descompila.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -23,5 +23,10 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 }
---
+++ src/main/java/br/com/descompila/api/medico/DadosListagemMedico.java
@@ -0,0 +1,12 @@
+package br.com.descompila.api.medico;
+
+public record DadosListagemMedico(
+        String nome,
+        String email,
+        String crm,
+        Especialidade especialidade
+) {
+    public DadosListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
