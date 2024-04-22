833c6fd381c40173f8925c7be9911df3c495f65f
Samuelson <samuelsonma@gmail.com>
2024-04-21 21:10:48 -0400

    atualização de medicos

--- src/main/java/br/com/descompila/api/controller/MedicoController.java
+++ src/main/java/br/com/descompila/api/controller/MedicoController.java
@@ -1,11 +1,11 @@
 package br.com.descompila.api.controller;
 
-import br.com.descompila.api.medico.DadosCadastroMedico;
-import br.com.descompila.api.medico.DadosListagemMedico;
-import br.com.descompila.api.medico.Medico;
-import br.com.descompila.api.medico.MedicoRepository;
+import br.com.descompila.api.medico.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +26,15 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
 }
