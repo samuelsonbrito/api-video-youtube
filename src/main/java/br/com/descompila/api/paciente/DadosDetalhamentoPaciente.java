54536a725986f38ccb09b1f1008104c1395eac5f
Samuelson <samuelsonma@gmail.com>
2024-05-01 18:59:40 -0400

    retorno do created e detalhamento

---
+++ src/main/java/br/com/descompila/api/paciente/DadosDetalhamentoPaciente.java
@@ -0,0 +1,13 @@
+package br.com.descompila.api.paciente;
+
+public record DadosDetalhamentoPaciente(
+        Long id,
+        String nome,
+        String email,
+        String cpf,
+        String telefone
+) {
+    public DadosDetalhamentoPaciente(Paciente paciente){
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+    }
+}
