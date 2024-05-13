cd5f7314f9f6ff5ee12bbf52086e1a2ff364da55
Samuelson <samuelsonma@gmail.com>
2024-05-12 23:06:43 -0400

    Adicionando Spring Security

--- src/main/java/br/com/descompila/api/infra/exception/TratadorDeErros.java
+++ src/main/java/br/com/descompila/api/infra/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package br.com.descompila.api.infra;
+package br.com.descompila.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
