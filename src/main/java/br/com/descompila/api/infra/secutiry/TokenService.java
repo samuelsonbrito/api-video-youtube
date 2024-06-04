7afceac6c066e83ee196a80d4908d1f2ebdbc8e4
Samuelson <samuelsonma@gmail.com>
2024-06-03 20:49:12 -0400

    Autenticando Spring Secutiry

--- src/main/java/br/com/descompila/api/infra/secutiry/TokenService.java
+++ src/main/java/br/com/descompila/api/infra/secutiry/TokenService.java
@@ -4,6 +4,7 @@ import br.com.descompila.api.domain.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -34,5 +35,18 @@ public class TokenService {
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 }
